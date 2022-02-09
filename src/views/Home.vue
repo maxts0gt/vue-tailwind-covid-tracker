2b8abcee338338823919e2765ca1516dd6628b02
Max <bazarsadtsogtjargal@gmail.com>
2022-02-10 03:00:30 +0900

    add data title component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <main v-if="!loading">Show Data</main>
+  <main v-if="!loading"><DataTitle :text="title" :dataDate="dataDate" /></main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
     <img :src="loadingImage" class="w-24 m-auto" alt="" />
@@ -7,9 +7,12 @@
 </template>
 
 <script>
+import DataTitle from '@/components/DataTitle';
 export default {
   name: 'Home',
-  components: {},
+  components: {
+    DataTitle,
+  },
   data() {
     return {
       loading: true,
